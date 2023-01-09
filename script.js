59a9f9c025cb4ade8445f64c5d75b0ee2c454ac0
Karim Ali <kariimalii96@gmail.com>
2023-01-09 17:43:17 +0200

    Modified the Task Done Button Functionality

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 $(function () {
     $("#btnAdd").click(() => {
         $taskText = $("#taskInput").val();
-        let taskBox = $(`<div class="task"></div>`);
-        let taskTextElm = $(`<div class="taskText">${$taskText}</div>`);
+        $("#taskInput").val("");
+        let taskBox = $(`<div class="taskBox"></div>`);
+        let taskTextElm = $(`<div class="task">${$taskText}</div>`);
         let doneBtn = $(`<button class="done">Done</button>`);
         let deleteBtn = $(`<button class="done">Delete</button>`);
         doneBtn.click(function() {
-            $(this).parent().addClass("TaskDone");
+            if($(this).parent().hasClass("TaskDone")) $(this).parent().removeClass("TaskDone");
+            else $(this).parent().addClass("TaskDone");
         })
         deleteBtn.click(function() {
             $(this).parent().remove();

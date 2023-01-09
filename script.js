29a5e1f6ea74291605b63b49fb580d56b43b2585
Karim Ali <kariimalii96@gmail.com>
2023-01-09 17:36:40 +0200

    added todo list functionality

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-/////// Code it yourself :)
+$(function () {
+    $("#btnAdd").click(() => {
+        $taskText = $("#taskInput").val();
+        let taskBox = $(`<div class="task"></div>`);
+        let taskTextElm = $(`<div class="taskText">${$taskText}</div>`);
+        let doneBtn = $(`<button class="done">Done</button>`);
+        let deleteBtn = $(`<button class="done">Delete</button>`);
+        doneBtn.click(function() {
+            $(this).parent().addClass("TaskDone");
+        })
+        deleteBtn.click(function() {
+            $(this).parent().remove();
+        })
+        taskBox.append(taskTextElm);
+        taskBox.append(doneBtn);
+        taskBox.append(deleteBtn);
+        $(".tasksBox").append(taskBox);
+    });
+
+});
+
+
